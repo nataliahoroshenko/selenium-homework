a8e1b3ab0899556578510917175e0c2d7d243fc9
natgoro <retankan@gmail.com>
2024-06-12 20:38:48 +0300

    initial commit

---
+++ src/test/java/com/demoqa/DemoQaTest.java
@@ -0,0 +1,10 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoQaTest {
+    @Test
+    void testButtonClick() {
+
+    }
+}
---
+++ src/test/java/org/example/FirstTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class FirstTest {
+    @Test
+    void smokeTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+        WebElement message = driver.findElement(By.id("message"));
+        message.getText();
+
+        driver.quit();
+    }
+
+}
